4b364885ec241fa2d866ff5bf947bf2b6722df14
EduGFE <edu21061991@gmail.com>
2021-11-24 18:39:50 +0100

    Test incroducido

---
+++ src/test/java/com/climbtogether/app/BootApplicationTest.java
@@ -0,0 +1,25 @@
+package com.climbtogether.app;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.climbtogether.climby.BootApplication;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = BootApplication.class)
+@TestPropertySource(value = "/application.properties")
+public class BootApplicationTest {
+
+	@Test
+	public void contextLoads() {
+	}
+	
+	@Test
+	public void main() {
+		BootApplication.main(new String[] {});
+	}
+
+}
