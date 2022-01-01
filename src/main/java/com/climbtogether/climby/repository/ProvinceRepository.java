78982eb7677fd7f13196cfdd167593abb006e7bc
EduGFE <edu21061991@gmail.com>
2022-01-01 14:46:09 +0100

    la buena

--- src/main/java/com/climbtogether/climby/repository/ProvinceRepository.java
+++ src/main/java/com/climbtogether/climby/repository/ProvinceRepository.java
@@ -11,17 +11,8 @@ import com.climbtogether.climby.domain.Province;
 @Repository
 public interface ProvinceRepository extends JpaRepository<Province, Integer> {
 	
-	
-//	@Query(value="select * from  tb_province tp",nativeQuery = true)
-//	List<Province> findAll();
-	
+
 	@Query(value="select tp.name_province, count(tt.province) as number_travels from tb_province tp  left join tb_travel tt on tp.name_province = tt.province where tt.departure_date >= CURRENT_DATE group by tp.name_province order by tp.name_province",nativeQuery = true)
 	List<Province> getProvincesWithTravelsLater();
-	
-//	@Query(value="select tp.name_province, count(tt.province) as number_travels from tb_province tp  left join tb_travel tt on tp.name_province = tt.province where tt.departure_date < CURRENT_DATE group by tp.name_province order by tp.name_province",nativeQuery = true)
-//	List<Province> getProvincesWithTravelsprevious();
-//	
-//	
-//	@Query(value="select tp.name_province, count(tt.province) as number_travels from tb_province tp left join tb_travel tt on tp.name_province = tt.province  group by tp.name_province order by tp.name_province",nativeQuery = true)
-//	List<Province> getProvincesWithTravels();
+
 }
