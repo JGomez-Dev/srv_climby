2bcb7b55915e26dfc636c06aa477ac0156d3c89e
Eduardo Gomez <edu21061991@gmail.com>
2023-01-24 20:45:39 +0100

    Despliegue en RailWay

--- src/main/java/com/climbtogether/climby/BootApplication.java
+++ src/main/java/com/climbtogether/climby/BootApplication.java
@@ -2,7 +2,7 @@ package com.climbtogether.climby;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
+
 
 
 @SpringBootApplication
--- src/test/java/com/climbtogether/climby/BootApplicationTest.java
+++ src/test/java/com/climbtogether/climby/BootApplicationTest.java
@@ -1,3 +1,4 @@
+/*
 package com.climbtogether.climby;
 
 
@@ -22,3 +23,4 @@ public class BootApplicationTest {
 //	}
 
 }
+*/
